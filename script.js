b75511cb8ae41b7e106e71e3303fa210f3602ba7
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-06 12:48:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function firstWord(s) {
   let s1 = s.split(" ");
   for(let i = 0; i <= s1.length; i++){
-	  let fw = s1[i][0];
-	  return fw;
+	  let fw = s1[0];
+	   return fw;
   } 
 } 
  
 // Do not change the code below
 
-/const s = prompt("Enter String:");
+const s = prompt("Enter String:");
 alert(firstWord(s));
