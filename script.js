4c0727f85340409ceb293700ce36165b9c7f935e
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-06 12:44:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function firstWord(s) {
   let s1 = s.split(" ");
   for(let i = 0; i <= s1.length; i++){
 	  let fw = s1[i][0];
-	  console.log(fw);
+	  return fw;
   } 
 } 
  
 // Do not change the code below
 
-const s = prompt("Enter String:");
+/const s = prompt("Enter String:");
 alert(firstWord(s));
